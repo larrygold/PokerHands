0447a33edfa3359497994c9456da35279dcab78b
Larry Goldstein <larry.goldstein@upslide.net>
2021-09-24 12:46:57 +0200

    2n, 3rd... highest cards checked

--- PokerHands.Tests/PokerHandsTests.cs
+++ PokerHands.Tests/PokerHandsTests.cs
@@ -14,6 +14,7 @@ namespace PokerHands.Tests
         [TestCase("2H 3D 5S 9C AS", "2H 3D 5S 9C KD", "Bob", TestName = "Ace Bob")]
         [TestCase("2H 3D 5S 9C KD", "2H 3D 5S 9C DS", "Bob", TestName = "K Bob")]
         [TestCase("2C 3H 4S 8C KD", "2H 3D 5S 9C KD", "John", TestName = "John 2nd highest")]
+        [TestCase("2C 3H 5S 9C KD", "2H 3D 4S 9C KD", "Bob", TestName = "Bob 3rd highest")]
         public void Wins_High_Card(string bobsHand, string johnsHand, string winnerExpected)
         {
             var winner = Poker.Compare(bobsHand, johnsHand);
