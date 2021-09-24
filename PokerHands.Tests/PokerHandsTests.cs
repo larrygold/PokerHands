cdf4c415695eea0dd9c85fd7dcfec13071d87424
Larry Goldstein <larry.goldstein@upslide.net>
2021-09-24 10:58:56 +0200

    Add project files.

---
+++ PokerHands.Tests/PokerHandsTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using PokerHands;
+
+namespace PokerHands.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            new Poker();
+        }
+    }
+}
